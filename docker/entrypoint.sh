23c7963cbec6ab99cfa45ce787eec9540ffce5ad
MLenterman <marcellenterman@hotmail.com>
2023-12-14 18:09:12 +0100

    build: switch to better suited G1GC garbarge collector

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 export JAVA_OPTS="$JAVA_OPTS \
 	-XX:HeapDumpPath=/usr/local/tomcat/logs \
 	-XX:+HeapDumpOnOutOfMemoryError \
-	-XX:+UseParallelGC \
+	-XX:+UseG1GC \
     -XX:+PrintFlagsFinal \
     -XshowSettings"
 
