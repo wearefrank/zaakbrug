4bbc2287291fd93b98e7c551473669bd046238cd
MLenterman <marcellenterman@hotmail.com>
2023-12-22 16:37:07 +0100

    build: garbage collector tuning to increase stability with large document processing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,20 @@ export JAVA_OPTS="$JAVA_OPTS \
 	-XX:HeapDumpPath=/usr/local/tomcat/logs \
 	-XX:+HeapDumpOnOutOfMemoryError \
 	-XX:+UseG1GC \
+	-XX:-ShrinkHeapInSteps \
+	-XX:+UnlockExperimentalVMOptions \
+	-XX:G1ReservePercent=20 \
+	-XX:InitiatingHeapOccupancyPercent=25 \
+	-XX:SurvivorRatio=32 \
+	-XX:+PerfDisableSharedMem \
+	-XX:+ParallelRefProcEnabled \
+	-XX:MaxTenuringThreshold=3 \
+	-XX:MaxGCPauseMillis=200 \
+	-XX:+UseStringDeduplication \
+	-XX:ConcGCThreads=4 \
+	-XX:MaxMetaspaceSize=256m \
+	-XX:G1HeapRegionSize=16m \
+	-XX:G1NewSizePercent=30 \
     -XX:+PrintFlagsFinal \
     -XshowSettings"
 
