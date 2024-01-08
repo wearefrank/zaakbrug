817cb836a24cc0a9a9c4444e0dae5ff2b7fd944d
Sergi Philipsen <philipsen.sergi@gmail.com>
2024-01-08 15:45:48 +0100

    fix: append user JAVA_OPTS to end of application JAVA_OPTS

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Append HeapDump and GC properties to existing JAVA_OPTS
-export JAVA_OPTS="$JAVA_OPTS \
+export JAVA_OPTS="\
 	-XX:HeapDumpPath=/usr/local/tomcat/logs \
 	-XX:+HeapDumpOnOutOfMemoryError \
 	-XX:+UseG1GC \
@@ -19,13 +19,14 @@ export JAVA_OPTS="$JAVA_OPTS \
 	-XX:MaxMetaspaceSize=256m \
 	-XX:G1HeapRegionSize=16m \
 	-XX:G1NewSizePercent=30 \
-    -XX:+PrintFlagsFinal \
-    -XshowSettings"
+	-XX:+PrintFlagsFinal \
+	-XshowSettings \
+	$JAVA_OPTS"
 
 # Enables writing of GC logs to /usr/local/tomcat/logs. Impacts performance!
 if [ ${GC_LOGGING_ENABLED:-false} == true ]
 	then
-		export JAVA_OPTS="$JAVA_OPTS -Xlog:codecache+sweep*=trace,class+unload,class+load,os+thread,safepoint,gc*,gc+stringdedup=debug,gc+ergo=trace,gc+age=trace,gc+phases=trace,gc+humongous=trace,jit+compilation=debug:file=/usr/local/tomcat/logs/zaakbrug-gc-log.log:level,tags,time,uptime:filesize=104857600,filecount=5"
+		export JAVA_OPTS="-Xlog:codecache+sweep*=trace,class+unload,class+load,os+thread,safepoint,gc*,gc+stringdedup=debug,gc+ergo=trace,gc+age=trace,gc+phases=trace,gc+humongous=trace,jit+compilation=debug:file=/usr/local/tomcat/logs/zaakbrug-gc-log.log:level,tags,time,uptime:filesize=104857600,filecount=5 $JAVA_OPTS"
 fi
 
 exec "$@"
