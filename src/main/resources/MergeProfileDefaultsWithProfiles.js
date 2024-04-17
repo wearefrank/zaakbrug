0bcdeabb0a22e974281c350e4638c9ce7987a8b2
MLenterman <marcellenterman@hotmail.com>
2024-04-17 10:40:32 +0200

    fix error when elements not present in profiles.json

--- src/main/resources/MergeProfileDefaultsWithProfiles.js
+++ src/main/resources/MergeProfileDefaultsWithProfiles.js
@@ -1,31 +1,3 @@
-// function mergeProfileDefaultsWithProfiles(profilesFile){
-//     const json = JSON.parse(profilesFile);
-//     const profileDefaults = {"profile": json["profileDefaults"]};
-//     const profiles = { "profile": json["profile"] };
-
-//     let result = {};
-//     result["profile"] = [];
-//     result["profileDefaults"] = json["profileDefaults"];
-//     for(const pf of profiles.profile){
-//         let pfres = {};
-
-//         Object.keys(profileDefaults.profile)
-//         .forEach(key => pfres[key] = profileDefaults.profile[key]);
-//         Object.keys(pf)
-//         .forEach(key => pfres[key] = pf[key]);
-
-//         pfres["valueOverrides"] = pf.valueOverrides
-//             .concat(profileDefaults.profile.valueOverrides
-//                     .filter((value) => pf.valueOverrides
-//                         .find((value2) => value2.key != value.key)));
-
-//         result.profile.push(pfres);
-//     }
-
-//     return JSON.stringify(result, null, 4);
-// }
-
-// Transpiled to ES5
 "use strict";
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
@@ -42,6 +14,9 @@ function mergeProfileDefaultsWithProfiles(profilesFile) {
   var result = {};
   result["profile"] = [];
   result["profileDefaults"] = json["profileDefaults"];
+  if (json["profileDefaults"] == null || json["profile"] == null) {
+    return profilesFiles;
+  }
   var _iterator = _createForOfIteratorHelper(profiles.profile),
     _step;
   try {
@@ -54,6 +29,10 @@ function mergeProfileDefaultsWithProfiles(profilesFile) {
       Object.keys(pf).forEach(function (key) {
         return pfres[key] = pf[key];
       });
+      if (pf["valueOverrides"] == null) {
+        result.profile.push(pfres);
+        return "break";
+      }
       pfres["valueOverrides"] = pf.valueOverrides.concat(profileDefaults.profile.valueOverrides.filter(function (value) {
         return pf.valueOverrides.find(function (value2) {
           return value2.key != value.key;
@@ -62,7 +41,8 @@ function mergeProfileDefaultsWithProfiles(profilesFile) {
       result.profile.push(pfres);
     };
     for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      _loop();
+      var _ret = _loop();
+      if (_ret === "break") break;
     }
   } catch (err) {
     _iterator.e(err);
