a5a353f9e642fe31a9de7e0edb167dc8b0165dbf
DelanoWAF <112384635+DelanoWAF@users.noreply.github.com>
2024-06-27 14:37:56 +0200

    Revert "merge from main"

--- docusaurus/docs/zaakbrug/_developer-guide/testing/unit-testing.md
+++
@@ -1,5 +0,0 @@
----
-sidebar_position: 100
----
-
-# Unit Testing
--- docusaurus/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import {Redirect} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export default function Home(): JSX.Element {
-  return <Redirect to={useBaseUrl('/docs/zaakbrug')} />;
-}
