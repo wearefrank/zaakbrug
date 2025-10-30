35b3b8c31acef9adf82eed6e81df353485ce9ca7
Meric Akgul <meric@wearefrank.nl>
2025-10-30 02:10:13 +0100

    Update JS file so that new data structure keeps working for merging profile values

--- src/main/resources/MergeProfileDefaultsWithProfiles-orig.js
+++ src/main/resources/MergeProfileDefaultsWithProfiles-orig.js
@@ -1,22 +1,29 @@
-function mergeProfileDefaultsWithProfiles(profilesFile){
-    const json = JSON.parse(profilesFile);
-    const profileDefaults = json["profileDefaults"];
-    const profiles = json["profile"];
+function mergeProfileDefaultsWithProfiles(profileDefaults, profiles) {
 
+    profileDefaults = JSON.parse(profileDefaults || '{}');
+    profiles = JSON.parse(profiles || '[]');
+    
     let result = {};
     result["profile"] = [];
     result["profileDefaults"] = profileDefaults;
 
-    if(profileDefaults == null || profiles == null || Object.keys(profileDefaults).length === 0){
-        return profilesFile;
+    if(profiles.length === 0 || Object.keys(profileDefaults).length === 0){
+        const returnData = {};
+        if(profiles.length > 0){
+            returnData["profile"] = profiles;
+        }
+        if(Object.keys(profileDefaults).length > 0){
+            returnData["profileDefaults"] = profileDefaults;
+        }
+        return JSON.stringify(returnData, null, 4);
     }
 
     for(const pf of profiles){
         let pfres = {};
         Object.keys(profileDefaults)
-        .forEach(key => pfres[key] = profileDefaults[key]);
+            .forEach(key => pfres[key] = profileDefaults[key]);
         Object.keys(pf)
-        .forEach(key => pfres[key] = pf[key]);
+            .forEach(key => pfres[key] = pf[key]);
 
         if(pf["valueOverrides"] == null || profileDefaults["valueOverrides"] == null){
             result.profile.push(pfres);
