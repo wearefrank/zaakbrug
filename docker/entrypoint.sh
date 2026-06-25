c54a9685913ba92cd8ceff5d746f955c10d22961
Jeroen Jansen van Rosendaal <jjansenvr@gmail.com>
2026-06-25 14:38:25 +0200

    Update GC logging condition in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,12 +14,14 @@ export JAVA_OPTS="\
 	--add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
 	$JAVA_OPTS"
 
-# By default, GC debugging is disabled. Enable it when dtap.stage is LOC.
-DTAP_STAGE="$(printenv 'dtap.stage' 2>/dev/null || true)"
-if [ "$DTAP_STAGE" = "LOC" ]; then
-	export JAVA_OPTS="-Xlog:gc*,gc+age=trace:file=/usr/local/tomcat/logs/gc-log.log:time,uptime:filesize=104857600,filecount=5 $JAVA_OPTS"
+# By default, GC debugging is disabled. Enable it when GC_LOGGING_ENABLED is LOC.
+if [ ${GC_LOGGING_ENABLED:-false} == true ]
+	then
+		export JAVA_OPTS="-Xlog:gc*,gc+age=trace:file=/usr/local/tomcat/logs/gc-log.log:time,uptime:filesize=104857600,filecount=5 $JAVA_OPTS"
 else
 	export JAVA_OPTS="-XX:+PerfDisableSharedMem $JAVA_OPTS"
 fi
+
+
 
 exec "$@"
